ce0a24aaacde2e8174cb6d8eca519812f0fa1d5a
mattnewell-cam <matthew_newell@outlook.com>
2025-12-17 12:07:14 +0000

    For debug=true, use neon development db rather than local

--- core/settings.py
+++ core/settings.py
@@ -15,6 +15,8 @@ import dj_database_url
 import os
 from django.core.exceptions import ImproperlyConfigured
 
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
