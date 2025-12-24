ce43bfada967994b1d52980bc50a95ae0cfc839c
mattnewell-cam <matthew_newell@outlook.com>
2025-12-24 15:36:54 +0000

    Bits and bobs.

--- portfolios/constants.py
+++ portfolios/constants.py
@@ -1,12 +1,12 @@
 BENCHMARK_CHOICES = [
     ("^GSPC",   "S&P 500"),
     ("^IXIC",   "NASDAQ Composite"),
-    ("^RUT",    "Russell 2000"),
-    ("^FTLC",   "FTSE 350"),
+    ("^RUT",    "Russell 2000 (US Mid"),
+    ("^FTLC",   "FTSE 350 (UK)"),
     ("^STOXXE", "STOXX Europe 600"),
-    ("1308.T",   "TOPIX"),
-    ("000300.SS", "CSI 300"),
-    ("^990100-USD-STRD", "MSCI World"),
-    ("ACWI",    "MSCI ACWI"),
+    ("1308.T",   "TOPIX (Japan)"),
+    ("000300.SS", "CSI 300 (China)"),
+    ("^990100-USD-STRD", "MSCI World (Developed)"),
+    ("ACWI",    "MSCI ACWI (All)"),
     ("EEM",     "MSCI Emerging Markets"),
 ]
